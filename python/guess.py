fd7d5a9697cbfacf21516f15b64aecc6d4fb2964
succsky <succsky@gmail.com>
2017-09-05 21:47:32 +0900

    9月5日

--- python/guess.py
+++ python/guess.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
  
 number = 23
-guess = int(raw_input('Enter an integer : '))
-if   guess == number:
-    print 'Congratulations, you guessed it.' # New block
-    print "(but you do not win any prizes!)" # New block
-
-elif guess < number:
-    print 'No,  it  is  a  little  higher  than  that'  
-else:
-    print 'No, it is a little lower than that' 
+running = 5
+while running:
+	guess = int(raw_input('Enter an integer : '))
+	if   guess == number:
+        	print 'Congratulations, you guessed it.' # New block
+        	print "(but you do not win any prizes!)" # New block
+		running =False
+	elif guess < number:
+    		print 'No,  it  is  a  little  higher  than  that'  
+	else:
+		print 'No, it is a little lower than that' 
 print 'Done'
---
+++ python/mymodule.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# Filename: mymodule.py
+def sayhi():
+    print 'Hi, this is mymodule speaking.'
+version = '0.1'
